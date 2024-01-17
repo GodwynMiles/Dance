a7a74f642b574402aeefe0d2cb2a942a62207046
spago <spago@DESKTOP-CSM61C1>
2024-01-17 14:41:28 +0800

    Add project files.

---
+++ DancePoints.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dance
+{
+    internal class DancePoints
+    {
+        public string name;
+        public int points;
+        public DancePoints(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        //overload the + operator
+        public static DancePoints operator +(DancePoints a, DancePoints b)
+        {
+            string joinName = a.name + " & " + b.name;
+            int totalScore = a.points + b.points;
+            DancePoints res = new DancePoints(joinName, totalScore);
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Dance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            int points1 = Convert.ToInt32(Console.ReadLine());
+            string name2 = Console.ReadLine();
+            int points2 = Convert.ToInt32(Console.ReadLine());
+
+            DancePoints dancer1 = new DancePoints(name1, points1);
+            DancePoints dancer2 = new DancePoints(name2, points2);
+
+            DancePoints total = dancer1 + dancer2;
+            Console.WriteLine(total.name);
+            Console.WriteLine(total.points);
+        }
+    }
+}
